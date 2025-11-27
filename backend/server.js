ce8b2e0ad0f82808001b098d82dc774ea4c3fca0
Ali2220 <alisarwar0277@gmail.com>
2025-11-27 15:48:59 +0500

    install and initialize socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const connectDB = require('./src/config/database')
-const dotenv = require('dotenv')
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const connectDB = require("./src/config/database");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+connectDB();
 
-connectDB()
+const app = express();
+const httpServer = createServer(app);
 
-app.listen(3000, () => {
-    console.log('Server is started');
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Code Collab Project'
+    })
+})
+
+io.on("connection", (socket) => {
+    console.log("User Connected", socket.id);
+    
+})
+
+httpServer.listen(3000, () => {
+    console.log('Server Started on port 3000');
+    console.log('Socket.IO server started.');
 })
